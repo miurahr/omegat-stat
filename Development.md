bf948e61600b0b0d0939343b0bde589d8c9d4789
Hiroshi Miura <miurahr@linux.com>
2021-01-08 09:20:58 +0900

    Check OmegaT stats
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ Development.md
@@ -0,0 +1,46 @@
+# Hint for development
+
+## Code in Main
+
+Install the dependencies
+
+```bash
+npm install
+```
+
+## Package for distribution
+
+GitHub Actions will run the entry point from the action.yml. Packaging assembles the code into one file that can be checked in to Git, enabling fast and reliable execution and preventing the need to check in node_modules.
+
+Actions are run from GitHub repos.  Packaging the action will create a packaged action in the dist folder.
+
+Run prepare
+
+```bash
+npm run prepare
+```
+
+Since the packaged index.js is run from the dist folder.
+
+```bash
+git add dist
+```
+
+## Create a release branch
+
+Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
+
+Checkin to the v1 release branch
+
+```bash
+git checkout -b v1
+git commit -a -m "v1 release"
+```
+
+```bash
+git push origin v1
+```
+
+Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
+
+Your action is now published! :rocket:
--- index.js
+++ index.js
@@ -1,21 +1,64 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require("@actions/github");
+const fs = require('fs');
 
+const token = core.getInput("token");
+const minCoverage = parseFloat(core.getInput("min-Coverage") || "0.0");
 
-// most @actions toolkit packages have async methods
+const statsfile = "omegat/project-stats.txt";
+
+function parse(data, line) {
+    return parseInt(data.split("\n")[line].split("\t")[1]);
+}
+
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    let data;
+    const statistics = {
+        sourceCount: 0,
+        targetCount: 0,
+        sourceCountWOD: 0,
+        targetCountWOD: 0,
+        summary: "",
+        coverage: 0,
+    };
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    try {
+        data = fs.readFileSync(statsfile, 'utf8').toString();
+        statistics.sourceCount = parse(data,4);
+        statistics.targetCount = parse(data, 5);
+        statistics.sourceCountWOD = parse(data, 6);
+        statistics.targetCountWOD = parse(data, 7);
+        statistics.coverage = statistics.targetCountWOD / statistics.sourceCountWOD * 100
+        statistics.summary = ` - " translated ${statistics.targetCountWOD} of ${statistics.sourceCountWOD}: total ${statistics.coverage}% w/o duplication.`;
+        core.info(statistics.summary);
+        core.setOutput('coverage', statistics.coverage.toString());
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    if (token && github.context.payload.head_commit) {
+        let conclusion;
+        if (!minCoverage) {
+            conclusion = "neutral";
+        } else if (statistics.coverage >= minCoverage) {
+            conclusion = "success";
+        } else {
+            conclusion = "failure";
+        }
+        github.getOctokit(token).checks.create({
+            owner: github.context.payload.repository.owner.login,
+            repo: github.context.payload.repository.name,
+            name: "omegat-stats-report",
+            head_sha: github.context.payload.head_commit.id,
+            status: "completed",
+            conclusion,
+            output: {
+                title: `${statistics.coverage.toFixed(0)}% coverage.`,
+                summary: statistics.summary + `, min-coverage: ${minCoverage}%`,
+                text: data,
+            },
+        });
+    }
 }
 
 run();
