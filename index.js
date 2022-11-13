66101201dd984db11b9eb0724d68e2601a0c5e79
Hiroshi Miura <miurahr@linux.com>
2022-11-13 17:24:46 +0900

    Drop octokit
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const core = require('@actions/core');
-const { Octokit } = require("@octokit/action");
 const fs = require('fs');
-
 
 function retrieve(data, line, col) {
     return data.split("\n")[line].split("\t")[col];
@@ -12,22 +10,21 @@ function parse(data, line, col) {
 }
 
 function makeRecord(title, data, line) {
-    let result = "| " + title + " ";
+    let result = "<tr><td>" + title + "</td>";
     for (let i = 1; i < 6; i++) {
-        result += "| " + retrieve(data, line, i) + " ";
+        result += "<td>" + retrieve(data, line, i) + "</td>";
     }
-    return (result + "|\n");
+    return (result + "</tr>");
 }
 
 function genDetailTotal(data) {
-    return "|  | Segments | Words | Characters(w/o spaces) | Characters(w/ spaces) | #Files |\n" +
-           "| :-- | --: | --: | --: | --: | --: |\n" +
+    return "<tr><th></th><th>Segments</th><th>Words</th><th>Characters(w/o spaces)</th></th>Characters(w/ spaces)</th><th>#Files</th></tr>" +
            makeRecord("Total", data, 1) + makeRecord("Remaining", data, 2) +
-           makeRecord("Unique", data, 3) + makeRecord("Unique remaining", data, 4) + "\n";
+           makeRecord("Unique", data, 3) + makeRecord("Unique remaining", data, 4);
 }
 
 function genDetailEach(data) {
-    let result = "| Filename | Segments | Words | Characters |\n| :-- | --: | --: | --: |\n";
+    let result = "<tr><th>Filename</th><th>Segments</th><th>Words</th><th>Characters</th><th></tr>";
     let detailLines = data.split("\n");
     try {
         for (let i = 3; i < detailLines.length; i++) {
@@ -38,15 +35,16 @@ function genDetailEach(data) {
             let progressS = (100 * (parseInt(item[3].trim()) - parseInt(item[4].trim())) / parseInt(item[3].trim())).toFixed(0);
             let progressW = (100 * (parseInt(item[7].trim()) - parseInt(item[8].trim())) / parseInt(item[7].trim())).toFixed(0);
             let progressC = (100 * (parseInt(item[11].trim()) - parseInt(item[12].trim())) / parseInt(item[11].trim())).toFixed(0);
-            result += `| ${item[0].trim()} | ![${progressS}%](https://progress-bar.dev/${progressS}/) |`;
-            result += ` ![${progressW}%](https://progress-bar.dev/${progressW}/) |`;
-            result += ` ![${progressC}%](https://progress-bar.dev/${progressC}/) |\n`;
+            result += `<tr><td>${item[0].trim()}</td><td><img alt="${progressS}%" src="https://progress-bar.dev/${progressS}/"></td>`;
+            result += `<td><img alt="${progressW}%" src="https://progress-bar.dev/${progressW}/"></td>`;
+            result += `<td><img alt="${progressC}%" src="https://progress-bar.dev/${progressC}/"></td></tr>`;
         }
     } catch(error) {
         core.setFailed(error.message);
     }
     return result;
 }
+
 
 async function run() {
     let data;
@@ -56,12 +54,11 @@ async function run() {
         sourceCountWOD: 0,
         targetCountWOD: 0,
         summary: "",
-        detail: "",
+        detailTotal: "",
+        detailEach: "",
         coverage: 0,
     };
 
-    const minCoverage = parseFloat(core.getInput("min-coverage") || "0.0");
-    const targetCoverage = parseFloat(core.getInput("target-coverage") || minCoverage)
     const statsfile = "omegat/project_stats.txt";
 
     try {
@@ -72,51 +69,16 @@ async function run() {
         stats.remainWOD = parse(data, 7, 1);
         let progress = stats.sourceWOD - stats.remainWOD
         stats.coverage = 100.0 * progress / stats.sourceWOD
-        stats.summary = ` - ![${stats.coverage.toFixed(0)}%](https://progress-bar.dev/${stats.coverage.toFixed(0)}/) `
-            + ` translated ${progress} of ${stats.sourceWOD}(${stats.coverage.toFixed(2)}%)`
-            + `, min: ${minCoverage}%, target ${targetCoverage}%`;
-        stats.detail = genDetailTotal(data.split("\n\n")[1]);
-        let detailBars = genDetailEach(data.split("\n\n\n")[1]);
-        if (stats.detail.length + detailBars.length < 65535) {
-            stats.detail += detailBars;
-        }
-        const htmlSummary = `<img src="https://progress-bar.dev/${stats.coverage.toFixed(0)}/"/>`
-            + ` translated ${progress} of ${stats.sourceWOD}(${stats.coverage.toFixed(2)}%)`
-            + `, min: ${minCoverage}%, target ${targetCoverage}%`;
-        core.info(htmlSummary);
+        stats.summary = ` - translated ${progress} of ${stats.sourceWOD}(${stats.coverage.toFixed(2)}%)`;
+        stats.detailTotal = genDetailTotal(data.split("\n\n")[1]);
+        stats.detailEach = genDetailEach(data.split("\n\n\n")[1]);
+        core.info(stats.summary);
         core.setOutput('coverage', stats.coverage.toString());
     } catch (error) {
         core.setFailed(error.message);
     }
-
-    let conclusion;
-    if (stats.coverage >= targetCoverage) {
-        conclusion = "success";
-    } else if (stats.coverage < minCoverage) {
-        conclusion = "failure";
-    } else {
-        conclusion = "neutral";
-    }
-
-    const octokit = new Octokit();
-    const head_commit = process.env.GITHUB_SHA;
-    const eventPayload = require(process.env.GITHUB_EVENT_PATH);
-    const repositoryId = eventPayload.repository.node_id;
-    const loginName = eventPayload.repository.owner.login;
-
-    octokit.checks.create({
-        owner: loginName,
-        repo: repositoryId,
-        head_sha: head_commit, 
-        name: "omegat-stats-report",
-        status: "completed",
-        conclusion: conclusion,
-        output: {
-            title: `${stats.coverage.toFixed(0)}% coverage.`,
-            summary: stats.summary,
-            text: stats.detail,
-        },
-    });
+    core.summary.addHeading(`${stats.coverage.toFixed(0)}% coverage.`, 3);
+    core.summary.addRaw(`<table>${stats.detailTotal}</table><br/><table>${stats.detailEach}</table>`)
 }
 
 run();
